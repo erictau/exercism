a67d0b2cf829da685aa547ba7a3fc10bf2053583
erictau <erictau04@gmail.com>
2023-03-23 20:37:28 -0700

    Complete exercise - resistor-color-duo

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,25 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+interface IColor {
+  [id: string]: string;
+}
+
+const colorMap: IColor = {
+  black: "0",
+  brown: "1",
+  red: "2",
+  orange: "3",
+  yellow: "4",
+  green: "5",
+  blue: "6",
+  violet: "7",
+  grey: "8",
+  white: "9",
+};
+
+export function decodedValue(colors: string[]): number {
+  const firstColor = colors[0];
+  const secondColor = colors[1];
+  colorMap;
+
+  const str = colorMap[firstColor] + colorMap[secondColor];
+  return parseInt(str);
 }
